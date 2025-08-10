c86b497b1dce5ccf279251709bafe35524930f44
Cara <carolina.nygren@gmail.com>
2025-08-10 15:05:52 +0200

    add gitignore and main.py title

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Hello World!")
-
-print("Yes, this is a Python script.")
-
-# This is a simple Python script that prints "Hello World!" to the console.
+"""
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Main script for ultrasound visual relationship between transducer diameter and 
+focal length.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
