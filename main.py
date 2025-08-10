a83cc6f9b4a1c191ea5b539c628a1eb155ec247a
Cara <carolina.nygren@gmail.com>
2025-08-10 15:11:40 +0200

    add requirements.txt

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ Main script for ultrasound visual relationship between transducer diameter and
 focal length.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 """
+import numpy as np
+import matplotlib.pyplot as plt
